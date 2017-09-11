86048541c308bac20f12faccfd65684961b01a8e
petercj1990 <petetjensen@gmail.com>
2017-09-11 15:44:47 -0600

    finished topo sort begin testing

--- packageInstaller.js
+++ packageInstaller.js
@@ -1,15 +1,58 @@
-(function(arg1){
-	if (arg1.constructor != Array){
-		return false;
-	};
-	var hierarchy = [][];
-	var packNum = 0;
-	for(var package in arg1){
-		var packageVal =  package.split(": ");
-		hierarchy[packNum] = packageVal[0]
-			for(var x = 1; x < packageVal.length; x++){
-				hierarchy[packNum].push(packageVal[x]);
-			}
-		packNum++;
+(function (){
+
+	function installer(arg1){
+		console.log('running test with: ', arg1);
+		if (arg1 == undefined){
+			return false;
+		};
+		var hierarchy = [];
+		var packNum = 0;
+		for(var package in arg1){
+			var packageVal =  package.split(":");
+			console.log('here be your package val', packageVal);
+			hierarchy[packNum] =  {pakage: packageVal[0], dep: packageVal[1], visited: false};
+			
+			packNum++;
+		}
+		console.log(hierarchy);
+		return topoSort(hierarchy);
 	}
-})
+	function topoSort(deps){
+
+		for(var x = 0; x < deps.lenght; x++){
+			deps[x].visited = false;
+		}
+		var route = "";
+		for (var x = 0; x < deps.lenght; x++){
+			if(deps[x].visited===false){
+				sortReccursion(deps, route, x);
+			}
+			console.log()
+			route += deps[x].package;
+		}
+		console.log(route);
+
+		return route;
+	}
+	function sortReccursion(dependencies, route, i){
+		console.log('recurring', route, i);
+		if(dependencies[i].visited === true){
+			return false;
+		}
+		dependencies[i].visited = true;
+		if(dependencies[i].dep = ""){
+			return true;
+		}
+		for(var x = 0; x < dependencies.lenght; x++){
+			if(dependencies[i].dep === dependencies[x].package ){
+				return sortReccursion(dependencies, route, x);
+			}
+		}
+
+	}
+	module.exports = {
+		installer: installer,
+		topoSort: topoSort,
+		sortReccursion: sortReccursion
+	}
+})()
--- test.js
+++ test.js
@@ -1,14 +1,33 @@
-(function(){
-	var installer = require('./packageInstaller.js');
-
-	if (installer() != false){
-		console.log()
-		return false;
-	};
-
-	if(installer([ "KittenService: CamelCaser", "CamelCaser: " ]) != "CamelCaser, KittenService"){
-		return false;
+(function(installer){
+	function tester () {
+		console.log(installer);
+		console.log('here i am');
+		if (installer.installer() != false){
+			console.log('failed on empty imput test');
+			return false;
+		};
+	
+		if(installer.installer(["KittenService: CamelCaser", "CamelCaser: " ]) != "CamelCaser, KittenService"){
+			console.log('failed on positive test 1');
+			return false;
+		}
+	
+		if(installer.installer(["KittenService: ",   "Leetmeme: Cyberportal",   "Cyberportal: Ice",   "CamelCaser: KittenService",   "Fraudstream: Leetmeme",   "Ice: " ]) != "KittenService, Ice, Cyberportal, Leetmeme, CamelCaser, Fraudstream"){
+			console.log('failed on positive test 2');
+			return false;
+		}
+	
+		if (installer.installer(["KittenService: ",   "Leetmeme: Cyberportal",   "Cyberportal: Ice",   "CamelCaser: KittenService",   "Fraudstream: ",   "Ice: Leetmeme" ] )!= false) {
+			console.log('failed on failure test');
+			return false;
+		}
+		return true;
 	}
 
-})
+	tester();
+
+	return{
+		tester
+	}
+})(require('./packageInstaller.js'));
 
